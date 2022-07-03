7a2bc97f9acbe3f8fa96661783caa73763070850
Kattupuchi <107940225+Kattupuchi@users.noreply.github.com>
2022-07-03 13:10:29 +0530

    Update start.py

--- Zaid/Player/start.py
+++ Zaid/Player/start.py
@@ -251,7 +251,7 @@ async def start(client: Client, message: Message):
                 InlineKeyboardButton("✘ ᴄʜᴀɴɴᴇʟ", url=f"https://t.me/{UPDATES_CHANNEL}"),
             ],
             [
-                InlineKeyboardButton("✘ ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ", url="https://github.com/Itz-Zaid/Zaid-Vc-Player"),
+                InlineKeyboardButton("✘ ᴍʏ ғᴀᴛʜᴇʀ", url="https://t.me/Kittu_the_criiminal"),
             ],
             [
                 InlineKeyboardButton("✘ ʜᴇʟᴘ ᴀɴᴅ ᴄᴏᴍᴍᴀɴᴅꜱ", callback_data="help"),
